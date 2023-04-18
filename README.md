ddf09d8ff20e0bb46eeddfb82a05036a6db4bcc5
Tristan Kuehn <tkuehn@uwo.ca>
2023-04-18 12:18:11 -0400

    Set up example repo

---
+++ README.md
@@ -0,0 +1,3 @@
+# Advanced git example repo
+
+This is an example repo to show off some branching and github actions.
---
+++ advanced_git/functions.py
@@ -0,0 +1,6 @@
+"""Some useful(?) functions in a module."""
+
+
+def add_one(num: float) -> float:
+    """Add one to a given number."""
+    return num + 1
