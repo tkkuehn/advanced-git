fd6e1fa91595a5e7eaab308be65b2b76d6b3d8af
Tristan Kuehn <tkuehn@uwo.ca>
2023-04-24 13:19:24 -0400

    Add add_two function

--- advanced_git/functions.py
+++ advanced_git/functions.py
@@ -4,3 +4,8 @@
 def add_one(num: float) -> float:
     """Add one to a given number."""
     return num + 1
+
+
+def add_two(num: float) -> float:
+    """Add two to a given number."""
+    return num + "2"
